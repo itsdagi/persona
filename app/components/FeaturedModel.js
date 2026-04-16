8be9459218af5053640d9027ef8ba5c7c3a0074e
itsdagi <dagm@persona.com>
2026-04-16 15:03:41 +0300

    updated 3d model build

--- app/components/FeaturedModel.js
+++ app/components/FeaturedModel.js
@@ -7,18 +7,26 @@ export default function FeaturedModel() {
 
   return (
     <section className={`section ${styles.featuredModel}`} ref={ref}>
-      <div className="container">
-        {/* 3D Interactive Model Embed */}
-        <div className={`${styles.modelWrapper} reveal`}>
-          <div className={styles.modelContainer}>
-            <iframe 
-              title="Modern Studio Apartment" 
-              frameBorder="0" 
-              allowFullScreen 
-              allow="autoplay; fullscreen; xr-spatial-tracking" 
-              src="https://sketchfab.com/models/68864965414f487090d1f677adbb8029/embed?autospin=1&autostart=1&preload=1&transparent=1&ui_infos=0&ui_watermark=0&ui_settings=0&ui_vr=0&ui_fullscreen=0&ui_animations=0&ui_help=0&ui_inspector=0&ui_stop=0&ui_theme=dark&dnt=1"
-            ></iframe>
-          </div>
+      <div className={styles.ambientGlow}></div>
+      
+      <div className={styles.wrapper}>
+        <div className={styles.hudElements}>
+          <div className={`${styles.crosshair} ${styles.tl}`}></div>
+          <div className={`${styles.crosshair} ${styles.tr}`}></div>
+          <div className={`${styles.crosshair} ${styles.bl}`}></div>
+          <div className={`${styles.crosshair} ${styles.br}`}></div>
+          <div className={styles.scanline}></div>
+          <div className={styles.hudText}>SYS.OBJ_VISUALIZER :: ONLINE</div>
+        </div>
+
+        <div className={styles.modelContainer}>
+          <iframe 
+            title="Modern Studio Apartment" 
+            frameBorder="0" 
+            allowFullScreen 
+            allow="autoplay; fullscreen; xr-spatial-tracking" 
+            src="https://sketchfab.com/models/68864965414f487090d1f677adbb8029/embed?autospin=0&autostart=1&preload=1&transparent=1&ui_infos=0&ui_watermark=0&ui_settings=0&ui_vr=0&ui_fullscreen=0&ui_animations=0&ui_help=0&ui_inspector=0&ui_stop=0&ui_theme=dark&dnt=1"
+          ></iframe>
         </div>
       </div>
     </section>
